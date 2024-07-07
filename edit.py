235be1c5cd3eb70840a689bd802f60ec483c2a7d
Lord-Lelsers <millan.kumar.24@bishops.com>
2024-07-07 02:06:58 -0400

    Save to video built into python file with argparse

--- edit.py
+++ edit.py
@@ -4,32 +4,30 @@ import cv2
 import numpy as np
 import json
 import alive_progress
+import subprocess
+
+
+TARGET_RATIO = 9 / 19.5 # width / height
 
 
 def main():
 	parser = argparse.ArgumentParser()
-	parser.add_argument("-f", "--folder", help="input folder", required=True)
-	parser.add_argument("-o", "--output", help="output folder", required=True)
-	parser.add_argument("-t", "--text", help="overlay text json", required=True)
+	parser.add_argument("-i", "--input",  help="input folder",         required=True)
+	parser.add_argument("-o", "--output", help="output folder",        required=True)
+	parser.add_argument("-t", "--text",   help="overlay text json",    required=True)
+	parser.add_argument("-v", "--video",  help="save output as video", required=False, action="store_true")
+	parser.add_argument("-f", "--fps",    help="video fps",            required=False, default=8, type=int)
+	parser.add_argument("-n", "--name",   help="video filename",       required=False, default="output.mp4")
 	args = parser.parse_args()
-
-
-	target_ratio = 9 / 19.5 # width / height
+	# ------------------------------------------------------------------------ #
 
 	try:
 		os.makedirs(args.output)
 	except FileExistsError:
 		pass
 
-	all_text = json.load(open(args.text))
-	current_text = {
-		"date": "",
-		"country": "",
-		"city": "",
-		"area": ""
-	}
-
-	ld = list(os.listdir(args.folder))
+	# ------------------------------------------------------------------------ #
+	ld = list(os.listdir(args.input))
 	ld.sort()
 	l = len(ld)
 	target_index = int(0.75 * l)
@@ -38,7 +36,7 @@ def main():
 
 	with alive_progress.alive_bar(l) as bar:
 		for filename in ld:
-			img = cv2.imread(os.path.join(args.folder, filename))
+			img = cv2.imread(os.path.join(args.input, filename))
 			width = img.shape[1]
 			try:
 				all_widths[width] += 1
@@ -46,20 +44,21 @@ def main():
 				all_widths[width] = 1
 			
 			bar()
+	# ------------------------------------------------------------------------ #
 
+	# ------------------------------------------------------------------------ #
 	target_width = 0
 	running_count = 0
 	keys = sorted(list(all_widths.keys()))
-	with alive_progress.alive_bar(len(all_widths)) as bar:
-		for width in keys:
-			count = all_widths[width]
-			next_count = running_count + count
-			if running_count < target_index <= next_count:
-				target_width = width
-			running_count = next_count
+	for width in keys:
+		count = all_widths[width]
+		next_count = running_count + count
+		if running_count < target_index <= next_count:
+			target_width = width
+		running_count = next_count
+	# ------------------------------------------------------------------------ #
 
-			bar()
-
+	# ------------------------------------------------------------------------ #
 	font = cv2.FONT_HERSHEY_SIMPLEX
 	font_size = int(0.00125 * target_width)
 	font_thickness = int(0.003 * target_width)
@@ -69,6 +68,14 @@ def main():
 	x = int(0.015 * target_width)
 	y = lambda i: int(0.025 * target_width + (i + 0.4) * font_size * 30)
 
+	all_text = json.load(open(args.text))
+	current_text = {
+		"date": "",
+		"country": "",
+		"city": "",
+		"area": ""
+	}
+
 	with alive_progress.alive_bar(l) as bar:
 		for filename in ld:
 			i = filename.split(".")[0]
@@ -77,18 +84,18 @@ def main():
 			except KeyError:
 				pass
 
-			img = cv2.imread(os.path.join(args.folder, filename))
+			img = cv2.imread(os.path.join(args.input, filename))
 
 			img_ratio = img.shape[1] / img.shape[0] # width / height
 
-			if img_ratio > target_ratio:
+			if img_ratio > TARGET_RATIO:
 				# wider ratio than target, background width = image width
 				background_width = img.shape[1]
-				background_height = int(background_width / target_ratio)
+				background_height = int(background_width / TARGET_RATIO)
 			else:
 				# taller ratio than target, background height = image height
 				background_height = img.shape[0]
-				background_width = int(background_height * target_ratio)
+				background_width = int(background_height * TARGET_RATIO)
 
 			img_output = np.zeros((background_height, background_width, 3), np.float32)
 
@@ -110,6 +117,16 @@ def main():
 			cv2.imwrite(os.path.join(args.output, output_filename), img_output)
 
 			bar()
+	# ------------------------------------------------------------------------ #
+
+	# ------------------------------------------------------------------------ #
+	if args.video:
+		# ffmpeg -framerate 10 -i output/%03d.jpg output.mp4
+		if args.output.endswith("/"): args.output = args.output[:-1]
+		cmd_str = f"ffmpeg -framerate {args.fps} -i {args.output}/%03d.jpg {args.name}"
+		cmd_lst = cmd_str.split(" ")
+		subprocess.run(cmd_lst)
+	# ------------------------------------------------------------------------ #
 
 
 
--- video-ify.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ffmpeg -framerate 10 -i output/%03d.jpg output.mp4
