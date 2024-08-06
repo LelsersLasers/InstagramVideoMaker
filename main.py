6a4909d6efaa6f713177a6ce7926f9f479b21a1e
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2024-08-05 18:02:28 -0700

    Preparing for no text + no rescale for Cancun video

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-i", "--input",  help="input folder",         required=True)
 	parser.add_argument("-o", "--output", help="output folder",        required=True)
-	parser.add_argument("-t", "--text",   help="overlay text json",    required=True)
+	# parser.add_argument("-t", "--text",   help="overlay text json",    required=True)
 	parser.add_argument("-v", "--video",  help="save output as video", required=False, action="store_true")
 	parser.add_argument("-f", "--fps",    help="video fps",            required=False, default=8, type=float)
 	parser.add_argument("-n", "--name",   help="video filename",       required=False, default="output.mp4")
@@ -52,44 +52,44 @@ def main():
 	# ------------------------------------------------------------------------ #
 
 	# ------------------------------------------------------------------------ #
-	target_width = 0
-	running_count = 0
-	keys = sorted(list(all_widths.keys()))
-	for width in keys:
-		count = all_widths[width]
-		next_count = running_count + count
-		if running_count < target_index <= next_count:
-			target_width = width
-		running_count = next_count
+	# target_width = 0
+	# running_count = 0
+	# keys = sorted(list(all_widths.keys()))
+	# for width in keys:
+	# 	count = all_widths[width]
+	# 	next_count = running_count + count
+	# 	if running_count < target_index <= next_count:
+	# 		target_width = width
+	# 	running_count = next_count
 	
-	print(f"Target width: {target_width}")
+	# print(f"Target width: {target_width}")
 	# ------------------------------------------------------------------------ #
 
 	# ------------------------------------------------------------------------ #
-	font = cv2.FONT_HERSHEY_SIMPLEX
-	font_size = int(0.00125 * target_width)
-	font_thickness = int(0.003 * target_width)
-	line_type = cv2.LINE_AA
-	font_color = (255, 255, 255)
+	# font = cv2.FONT_HERSHEY_SIMPLEX
+	# font_size = int(0.00125 * target_width)
+	# font_thickness = int(0.003 * target_width)
+	# line_type = cv2.LINE_AA
+	# font_color = (255, 255, 255)
 
-	x = int(0.05 * target_width)
-	y = lambda i: int(0.08 * target_width + (i + 0.4) * font_size * 30)
+	# x = int(0.05 * target_width)
+	# y = lambda i: int(0.08 * target_width + (i + 0.4) * font_size * 30)
 
-	all_text = json.load(open(args.text))
-	current_text = {
-		"date": "",
-		"location": "",
-		"area": "",
-		"comment": ""
-	}
+	# all_text = json.load(open(args.text))
+	# current_text = {
+	# 	"date": "",
+	# 	"location": "",
+	# 	"area": "",
+	# 	"comment": ""
+	# }
 
 	with alive_progress.alive_bar(l) as bar:
 		for filename in ld:
 			i = filename.split(".")[0]
-			try:
-				current_text.update(all_text[i])
-			except KeyError:
-				pass
+			# try:
+			# 	current_text.update(all_text[i])
+			# except KeyError:
+			# 	pass
 
 			img = cv2.imread(os.path.join(args.input, filename))
 
@@ -112,13 +112,13 @@ def main():
 
 			img_output[y_offset:y_offset + img.shape[0], x_offset:x_offset + img.shape[1]] = img
 
-			resize_ratio = target_width / background_width
-			img_output = cv2.resize(img_output, (target_width, int(background_height * resize_ratio)))
+			# resize_ratio = target_width / background_width
+			# img_output = cv2.resize(img_output, (target_width, int(background_height * resize_ratio)))
 
-			cv2.putText(img_output, current_text["date"],     (x, y(0)), font, font_size, font_color, font_thickness, line_type)
-			cv2.putText(img_output, current_text["location"], (x, y(1)), font, font_size, font_color, font_thickness, line_type)
-			cv2.putText(img_output, current_text["area"],     (x, y(2)), font, font_size, font_color, font_thickness, line_type)
-			cv2.putText(img_output, current_text["comment"],  (x, y(3)), font, font_size, font_color, font_thickness, line_type)
+			# cv2.putText(img_output, current_text["date"],     (x, y(0)), font, font_size, font_color, font_thickness, line_type)
+			# cv2.putText(img_output, current_text["location"], (x, y(1)), font, font_size, font_color, font_thickness, line_type)
+			# cv2.putText(img_output, current_text["area"],     (x, y(2)), font, font_size, font_color, font_thickness, line_type)
+			# cv2.putText(img_output, current_text["comment"],  (x, y(3)), font, font_size, font_color, font_thickness, line_type)
 
 			output_filename = filename.split(".")[0] + ".jpg"
 			cv2.imwrite(os.path.join(args.output, output_filename), img_output)
@@ -128,7 +128,6 @@ def main():
 
 	# ------------------------------------------------------------------------ #
 	if args.video:
-		# ffmpeg -framerate 10 -i output/%03d.jpg output.mp4
 		if args.output.endswith("/"): args.output = args.output[:-1]
 		cmd_str = f"ffmpeg -framerate {args.fps} -i {args.output}/%03d.jpg {args.name}"
 		cmd_lst = cmd_str.split(" ")
